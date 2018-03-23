905330b51217e3b8528fac5de1eea26af7645616
David R. Bild <david.bild@xaptum.com>
2018-03-23 16:19:38 -0500

    amcl: upgrade to 4.7.0 pre-release

--- Formula/amcl.rb
+++ Formula/amcl.rb
@@ -1,20 +1,20 @@
 class Amcl < Formula
   desc "A standards compliant C cryptographic library with no external dependencies, specifically designed to support the Internet of Things (IoT)."
   homepage "https://github.com/milagro-crypto/milagro-crypto-c"
-  url "https://github.com/milagro-crypto/milagro-crypto-c/archive/7e0f5f9198dddd3d84c85e2aac720b318038c378.tar.gz"
-  sha256 "c3905c74631694ccb51c12e02b0ef3163e920f648de25cd5f3b776c14356ff72"
+  url "https://github.com/milagro-crypto/milagro-crypto-c/archive/cf38b08480ef9ed546ff82b4939292bd0d218540.tar.gz"
+  sha256 "06e4f46557da66e679e75b175765843a30c9c85b8048eb4c3b947fafb9369840"
 
   depends_on "cmake" => :build
 
   def install
     mkdir "build" do
       system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args,
-             "-DAMCL_HEADER_INSTALL_SUBDIR=amcl",
-             "-DAMCL_CHUNK=64",
-             "-DBUILD_MPIN=ON",
-             "-DBUILD_WCC=ON",
-             "-DAMCL_RSA=2048,3072,4096",
-             "-DAMCL_CURVE=NIST256,C25519,ED25519,BRAINPOOL,ANSSI,NUMS256E,NUMS256W,NUMS384E,NUMS384W,NUMS512E,NUMS512W,HIFIVE,GOLDILOCKS,NIST384,C41417,NIST521,BN254,BN254CX,BLS383,FP256BN,FP512BN,BLS461"
+             "-DAMCL_INCLUDE_SUBDIR=amcl",
+             "-DBUILD_BENCHMARKS=OFF",
+             "-DBUILD_DOCS=OFF",
+             "-DBUILD_EXAMPLES=OFF",
+             "-DBUILD_PYTHON=OFF",
+             "-DBUILD_TESTING=OFF"
       system "make"
       system "make", "install"
     end
