a7a218a30d1dc46adeeb083167be7d06d6cd4a8f
David R. Bild <david.bild@xaptum.com>
2018-04-06 08:19:01 -0500

    amcl: update to final 4.7.0 release

--- Formula/amcl.rb
+++ Formula/amcl.rb
@@ -2,8 +2,8 @@ class Amcl < Formula
   desc "A standards compliant C cryptographic library with no external dependencies, specifically designed to support the Internet of Things (IoT)."
   homepage "https://github.com/milagro-crypto/milagro-crypto-c"
   version "4.7.0"
-  url "https://github.com/milagro-crypto/milagro-crypto-c/archive/42111ad79eb41bb198032902a78f7cc49826e1bd.tar.gz"
-  sha256 "4e2e32ff6db36abe11ec2ab07acb4d1fb30a1ad656bf3cdef66ccbbc223ad750"
+  url "https://github.com/milagro-crypto/milagro-crypto-c/archive/4.7.0.tar.gz"
+  sha256 "1c17bfbc384723b25a274091082c263ff87453a4e347a50d96c174defaddc979"
 
   depends_on "cmake" => :build
 
