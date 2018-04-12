206f02e93730e0156a65b987f4bbbbe5fc62ad3c
David R. Bild <david.bild@xaptum.com>
2018-04-12 17:04:13 -0500

    xtt: add formula at v0.5.0

---
+++ Formula/xtt.rb
@@ -0,0 +1,29 @@
+class Xtt < Formula
+  desc "C lib for XTT Trusted Transit Protocol for securing the IoT"
+  homepage "https://github.com/xaptum/xtt"
+  url "https://github.com/xaptum/xtt/archive/v0.5.0.tar.gz"
+  sha256 "4a2c1cffdb2b0a89d206a9984ac82d0822681d01244dc33c697652004f5f9b1e"
+
+  depends_on "cmake" => :build
+  depends_on "ecdaa"
+  depends_on "libsodium"
+  depends_on "xaptum-tpm" => :recommended
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_EXAMPLES=OFF"
+    args << "-DBUILD_TESTING=OFF"
+
+    if build.without? "xaptum-tpm"
+      args << "-DUSE_TPM=OFF"
+    else
+      args << "-DUSE_TPM=ON"
+    end
+
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+end
