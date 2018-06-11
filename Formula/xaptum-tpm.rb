383fc342a4098322bf5d829706f31f317db38d8a
Zane Beckwith <zanebeckwith@aol.com>
2018-06-11 11:11:33 -0500

    Upgrade xaptum-tpm to v0.5.5

--- Formula/xaptum-tpm.rb
+++ Formula/xaptum-tpm.rb
@@ -1,8 +1,8 @@
 class XaptumTpm < Formula
   desc "Utilities for interacting with the TPM 2.0 chip used to access the Xaptum Edge Network Fabric."
   homepage "https://github.com/xaptum/xaptum-tpm"
-  url "https://github.com/xaptum/xaptum-tpm/archive/v0.5.2.tar.gz"
-  sha256 "78985423adde92d7fd09f4c13653f48ae7abab953e1ed901663eb90acb3a8351"
+  url "https://github.com/xaptum/xaptum-tpm/archive/v0.5.5.tar.gz"
+  sha256 "8631f6539c1e90e42deebba7c2095aa657b175542eb87e1a4e6478c8d89433db"
 
   depends_on "cmake" => :build
 
