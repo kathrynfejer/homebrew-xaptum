df4ce24980244e9aeb6a10ca88439eb131599509
Unknown <Kathryn.fejer@xaptum.com>
2018-07-25 11:49:16 -0500

    Upgrade to xtt v0.9.1 and xtt-cpp v0.2.0

--- Formula/xtt-cpp.rb
+++ Formula/xtt-cpp.rb
@@ -1,8 +1,15 @@
 class XttCpp < Formula
+<<<<<<< master
   desc "C++ wrappers for the XTT IoT Security Protocol"
   homepage "https://github.com/xaptum/xtt-cpp"
   url "https://github.com/xaptum/xtt-cpp/archive/v0.1.0.tar.gz"
   sha256 "b4a1574ccd0d6b9ffbf2b66a316013147b4f203f03ab96e5c908bedceda9e331"
+=======
+  desc "C++ lib for XTT Trusted Transit Protocol for securing the IoT"
+  homepage "https://github.com/xaptum/xtt-cpp"
+  url "https://github.com/xaptum/xtt-cpp/archive/v0.2.0.tar.gz"
+  sha256 "b13e6832cfbe2125a49f71508400cf04028331c35417e3f69bedfa5a9bd17bd5"
+>>>>>>> Upgrade to xtt v0.9.1 and xtt-cpp v0.2.0
 
   depends_on "cmake" => :build
   depends_on "xtt"
